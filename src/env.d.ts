9c0f41bb36fd32bfacf6e815cb7c0d0ccbf2400d
Andrew Schastlivcev <dj_winston@outlook.com>
2024-01-03 02:11:47 +0200

    Update pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="astro/client" />
 
 declare function onloadTurnstileCallback(): void;
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  readonly PUBLIC_CF_SITE_KEY: string;
+  readonly PUBLIC_CF_SECRET_KEY: string;
+}
